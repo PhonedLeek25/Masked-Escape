8c1c99df2dc8d0f4b70fab79da94ff94c3cc4974
Deniz Alp Güdük <165922967+Deniz-GDK@users.noreply.github.com>
2026-01-30 20:12:51 +0300

    small changes

--- Assets/Dev/Scripts/NPCNav.cs
+++ Assets/Dev/Scripts/NPCNav.cs
@@ -1,18 +1,57 @@
 using UnityEngine;
 
+public enum CurrentMask { mask1 , mask2 };
 public class NPCNav : MonoBehaviour
 {
+    //mask 1: attracts NPC A, Repells B, neutral C
+    // mask 2: attracts NPC C, Repells A, neutral B
     public enum NPCTypes { A, B, C };
-    public NPCTypes ThisNPCType;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [Header("NPC based references")]
+    public NPCTypes ThisNPCType;
+
+    [Header("Player based references")]
+    public CurrentMask currentMask;
+    public GameObject playerObj;
+
+
+    public void TakeActionBasedOnCurrentMask()
     {
-        
+        switch (currentMask)
+        {
+            case CurrentMask.mask1:
+                if (ThisNPCType == NPCTypes.A)
+                {
+                    //Attract to player
+                }
+                else if (ThisNPCType == NPCTypes.B)
+                {
+                    //Repell from player
+                }
+                else
+                {
+                    //Neutral behavior
+                }
+                break;
+            case CurrentMask.mask2:
+                if (ThisNPCType == NPCTypes.C)
+                {
+                    //Attract to player
+                }
+                else if (ThisNPCType == NPCTypes.A)
+                {
+                    //Repell from player
+                }
+                else
+                {
+                    //Neutral behavior
+                }
+                break;
+        }
     }
+    private void Update()
+    {
+        TakeActionBasedOnCurrentMask();
+    }
+    
 }
